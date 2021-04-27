23cb3f407f1fb60f09824014b223097ae90b8b07
Luciano Sousa <luciano.sousa@shopify.com>
2021-04-27 11:48:55 -0400

    Add linux script

---
+++ linux-startup.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+set -e
+
+sudo apt-get install -y ripgrep
+
+dotfiles=(.gemrc .vimrc .bashrc .bash_profile .git-completion.bash .git-prompt.sh .gitignore_global)
+
+for file in "${dotfiles[@]}"
+do
+  echo "▶︎ Linking ${file}"
+  rm -f ~/$file
+  ln -s ~/dotfiles/files/$file ~/$file
+done
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
